068164b6f641f972d7dd0ce2d54796cfe9aef639
YusuffAhmad <97904018+YusuffAhmad@users.noreply.github.com>
2022-01-30 22:49:03 +0100

    Chapter 12 Question 12 and 13

---
+++ Question12/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Question12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the fullpath of the file to read from: ");
+            string file = Console.ReadLine();
+            ValidiateFile(file);
+        }
+        public static void ValidiateFile(string file)
+        {
+            if (File.Exists(file))
+            {
+                using (StreamReader reader = new StreamReader(file)) 
+                {
+                    var result = reader.ReadLine();
+                    Console.WriteLine(result);
+                }
+            }
+            else if (!File.Exists(file))
+            {
+                throw new FileNotFoundException();
+            }
+        }
+    }
+}
